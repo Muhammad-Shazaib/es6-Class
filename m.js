35bd04a3c8dcb3a66a701508cefd1db527b84a3b
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-07-06 21:08:44 +0500

    updated

--- m.js
+++ m.js
@@ -10,7 +10,10 @@ class Book {
         this.author = author;
         this.genre = genre;
     }
-
+ static sold = 5
+ static viewIncrement() {
+     console.log('a view is incremented');
+ }
 }
 
 
@@ -20,3 +23,33 @@ let b = new Book('Harry Potter', 'J.K. Rowling', 'Fiction'); //Object
 console.log(b.name);
 console.log(b.author);
 console.log(b.genre);
+console.log(Book.sold); // static method call
+console.log(Book.viewIncrement); // static method call
+
+
+function Foo() {  // named function
+    console.log('This is a named function');
+}
+
+
+let zee = function () {  // anonymous function
+    console.log('This is an anonymous function');
+};
+
+
+
+
+class Risala extends Book {
+    constructor(name, author, genre, weekNumber, com) {
+        console.log('this type is getting constructed');
+        super(name, author, genre); // Call the parent class constructor
+       
+        this.author = author;
+        this.week = weekNumber;
+        this.company = com;
+    }
+
+}
+
+let abc = new Risala('Risala', 'J.K. Rowling', 'politics', 2, 'express');
+console.log(abc.genre);
